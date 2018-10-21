2f843b8111b6c4bc954bf371a411c21eae53d295
Logic <yan1465781407@163.com>
2018-10-21 17:18:42 +0800

    第一次提交一个空的的项目

---
+++ src/main/java/example/controller/IndexController.java
@@ -0,0 +1,14 @@
+package example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("进入方法");
+        return "index";
+    }
+}
